901183617001d0cc59dc5a4040dd98b2168ad1ad
Gustavo Sillero <gustavo.sillero@iwsbrazil.com>
2016-04-08 22:47:33 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const semver = require('semver');
+const fetch = require('node-fetch');
+const url = require('url');
+
+module.exports = pkg => {
+  if (typeof pkg === 'string') {
+    pkg = {
+      name: pkg
+    }
+  }
+
+  if (!pkg || !pkg.name) {
+    throw new Error('You must provide a package name');
+  }
+  
+  pkg.version = pkg.version || '*';
+  
+  if (!semver.validRange(pkg.version)) {
+    throw new Error('That is not a valid package version range');
+  } 
+  
+  pkg.registry = pkg.registry || 'http://registry.npmjs.com/'; 
+  
+  return fetch(url.resolve(pkg.registry, pkg.name))
+    .then(response => response.json())
+    .then(data => semver.maxSatisfying(Object.keys(data.versions || {}), pkg.version))
+    .catch(err => { throw new Error(err); });
+}
