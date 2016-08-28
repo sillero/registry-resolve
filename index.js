b91384ff9e6adb7d00afb166bb18e1a55e4d7286
Will Hilton <wmhilton@gmail.com>
2016-08-28 01:44:30 -0400

    Add 'full' option to get all package details not just version number.

--- index.js
+++ index.js
@@ -14,17 +14,20 @@ module.exports = pkg => {
   if (!pkg || !pkg.name) {
     throw new Error('You must provide a package name');
   }
-  
+
   pkg.version = pkg.version || '*';
-  
+
   if (!semver.validRange(pkg.version)) {
     throw new Error('That is not a valid package version range');
-  } 
-  
-  pkg.registry = pkg.registry || 'http://registry.npmjs.com/'; 
-  
+  }
+
+  pkg.registry = pkg.registry || 'http://registry.npmjs.com/';
+
   return fetch(url.resolve(pkg.registry, pkg.name))
     .then(response => response.json())
-    .then(data => semver.maxSatisfying(Object.keys(data.versions || {}), pkg.version))
+    .then(data => {
+      let v = semver.maxSatisfying(Object.keys(data.versions || {}), pkg.version);
+      return (pkg.full) ? data.versions[v] : v;
+    })
     .catch(err => { throw new Error(err); });
 }
